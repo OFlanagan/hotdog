66e099ae5dcfbdaa408d402560ece80e0acb07c2
Owen Flanagan <owenflanagan44@gmail.com>
2020-08-23 18:52:42 +1200

    removing more lines from utils.py

--- utils.py
+++ utils.py
@@ -1,18 +1,2 @@
-# Numpy and pandas by default assume a narrow screen - this fixes that
 from fastai2.vision.all import *
-from nbdev.showdoc import *
 from ipywidgets import widgets
-from pandas.api.types import CategoricalDtype
-
-import matplotlib as mpl
-# mpl.rcParams['figure.dpi']= 200
-mpl.rcParams['savefig.dpi']= 200
-mpl.rcParams['font.size']=12
-
-set_seed(42)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
-pd.set_option('display.max_columns',999)
-np.set_printoptions(linewidth=200)
-torch.set_printoptions(linewidth=200)
-
